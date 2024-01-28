ce4599aa0937cc1d120444692065ad3b0cacf431
denisk13005 <kirmanndenis@gmail.com>
2024-01-28 14:18:55 +0100

    test

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    id page
-    <!-- <div v-for="post in posts" :key="post">
-      {{ post }}
-    </div> -->
+    <pre v-if="movie">{{ movie }}</pre>
   </div>
 </template>
 
@@ -11,32 +8,35 @@
 export default {
   data() {
     return {
-      movie: [],
+      movie: null,
     }
   },
 
-  async fetch({ params }) {
-    const id = params.id
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGU5OGZiM2NhZDI3MTI3NTdmNGZkMTAyMDVlZDk4MiIsInN1YiI6IjYxZmUzYjhmZDA1MWQ5MDBkOWJjNGQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O4b4AJGaL_DAH5r7Ullr1NH1vqpRRRh9W5NuHK8u0mY',
-      },
-    }
+  async mounted() {
+    try {
+      const id = this.$route.params.id // Si vous utilisez Vue Router
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGU5OGZiM2NhZDI3MTI3NTdmNGZkMTAyMDVlZDk4MiIsInN1YiI6IjYxZmUzYjhmZDA1MWQ5MDBkOWJjNGQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O4b4AJGaL_DAH5r7Ullr1NH1vqpRRRh9W5NuHK8u0mY',
+        },
+      }
 
-    this.movie = await fetch(
-      `https://api.themoviedb.org/3/list/${id}?language=en-US&page=1`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err))
+      const response = await fetch(
+        `https://api.themoviedb.org/3/list/${id}?language=en-US&page=1`,
+        options
+      )
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      this.movie = await response.json()
+      console.log(this.movie, 'movie')
+    } catch (error) {
+      console.error('Fetch error:', error)
+    }
   },
 }
 </script>
 
-
-<style lang="scss" scoped>
-</style>
