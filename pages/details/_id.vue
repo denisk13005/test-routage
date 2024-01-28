00c71e7d6abe4d3f23ab584dc6f7a4682348fab0
denisk13005 <kirmanndenis@gmail.com>
2024-01-28 16:47:31 +0100

    test new method

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -12,30 +12,36 @@ export default {
     }
   },
 
-  async mounted() {
-    try {
-      const id = this.$route.params.id // Si vous utilisez Vue Router
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGU5OGZiM2NhZDI3MTI3NTdmNGZkMTAyMDVlZDk4MiIsInN1YiI6IjYxZmUzYjhmZDA1MWQ5MDBkOWJjNGQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O4b4AJGaL_DAH5r7Ullr1NH1vqpRRRh9W5NuHK8u0mY',
-        },
-      }
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      console.log('titi')
+      try {
+        const id = this.$route.params.id // Si vous utilisez Vue Router
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGU5OGZiM2NhZDI3MTI3NTdmNGZkMTAyMDVlZDk4MiIsInN1YiI6IjYxZmUzYjhmZDA1MWQ5MDBkOWJjNGQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O4b4AJGaL_DAH5r7Ullr1NH1vqpRRRh9W5NuHK8u0mY',
+          },
+        }
 
-      const response = await fetch(
-        `https://api.themoviedb.org/3/list/${id}?language=en-US&page=1`,
-        options
-      )
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
+        const response = await fetch(
+          `https://api.themoviedb.org/3/list/${id}?language=en-US&page=1`,
+          options
+        )
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        this.movie = await response.json()
+        console.log(this.movie, 'movie')
+      } catch (error) {
+        console.error('Fetch error:', error)
       }
-      this.movie = await response.json()
-      console.log(this.movie, 'movie')
-    } catch (error) {
-      console.error('Fetch error:', error)
-    }
+    },
   },
 }
 </script>
